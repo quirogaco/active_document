cc0ff448be02fb0688019352cd947ce502f13fdd
Juan Carlos Rodríguez Ospina <quirogaco@gmail.com>
2022-11-05 15:05:28 -0400

    update env

--- web/ui/vite.config.js
+++ web/ui/vite.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const fs = require('fs');
 import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import DefineOptions from 'unplugin-vue-define-options/vite';
 
 let ubicacion = "../../";
 let ambiente = loadEnv("development", ubicacion, prefix = 'CFG_')
@@ -11,7 +10,6 @@ export default defineConfig({
     plugins: [
         //basicSsl(),
         //HttpsCert(),
-        DefineOptions(),
         vue()
     ],
     resolve: {
